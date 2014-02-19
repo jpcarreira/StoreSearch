b2a147a9f3f92ab62a0b4aad368d92b559f584ec
jpcarreira <jpmavcarreira@gmail.com>
2014-02-19 19:17:06 +0000

    added NothingFoundCell class (sub from UITableViewCell) ref #1

---
+++ StoreSearch/StoreSearch/NothingFoundCell.h
@@ -0,0 +1,15 @@
+//
+//  NothingFoundCell.h
+//  StoreSearch
+//
+//  Created by João Carreira on 19/02/14.
+//  Copyright (c) 2014 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// class added just to overcome the "Unknown class 'NothingFoundCell' in Interface Builder file" warning during runtime
+
+@interface NothingFoundCell : UITableViewCell
+
+@end
